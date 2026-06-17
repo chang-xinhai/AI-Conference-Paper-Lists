4934035b1603aeefd80455522c1c087454ad7019
chang-xinhai <changxinhai@stu.pku.edu.cn>
2026-06-17 16:36:33 +0800

    data: switch corl proceedings to pmlr

--- src/aicpl/sources/pmlr.py
+++ src/aicpl/sources/pmlr.py
@@ -49,6 +49,11 @@ PMLR_VOLUMES: dict[tuple[str, int], str] = {
     ("automl", 2023): "v224",
     ("automl", 2024): "v256",
     ("automl", 2025): "v293",
+    ("corl", 2021): "v164",
+    ("corl", 2022): "v205",
+    ("corl", 2023): "v229",
+    ("corl", 2024): "v270",
+    ("corl", 2025): "v305",
 }
 
 
